737988ea6ba6cae01d0d55b7fcb6125ec46dea24
Leonardo Bonfanti <bonfanti.leonardo@gmail.com>
2019-12-04 23:56:17 -0300

    Selenium
    Instalação pacote selenium

--- ProcobCrawler/ProcobCrawler/Program.cs
+++ ProcobCrawler/ProcobCrawler/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace ProcobCrawler
 {
@@ -6,7 +8,7 @@ namespace ProcobCrawler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
